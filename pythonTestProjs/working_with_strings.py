e4a4883b2256d335c94678ad6d7e37a64ea6e984
siddharthpangotra <siddharthpangotra@gmail.com>
2020-01-12 13:02:21 +0530

    Create working_with_strings.py
    this file aims to explain some of the string operations in python

---
+++ pythonTestProjs/working_with_strings.py
@@ -0,0 +1,11 @@
+# working with string
+message = 'yellow bird'
+message2 = "up in the sky"
+# print('{}, {}.'.format(message, message2))
+# print(f'{message}, {message2}.')
+
+# the below ones will print all the possible operations with string 'message'
+# print(dir(message))
+# print(help(str))
+#print(help(str.expandtabs))
+#sprint(message.expandtabs(tabsize=16))

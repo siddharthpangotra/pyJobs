408f1e2bb536caa7c976970bf7b1d8376ec066a2
siddharthpangotra <siddharthpangotra@gmail.com>
2020-01-13 22:01:23 +0530

    Create working_with_numbers.py
    This file includes some operations on integer values and casting of variables from int to string and vice-versa

---
+++ pythonTestProjs/working_with_numbers.py
@@ -0,0 +1,16 @@
+num = 3.75
+num2 = 3.70
+# print(type(num))
+#print(round(num, 1))
+#print(num == num2)
+num3 = '100'
+num4 = 200
+#print(num3 + num4)
+
+#-_-_-_Casting-_-_-
+
+num5 = int(num3)
+#print(num5 + num4)
+# print(type(num5))
+num6 = str(num4)
+#print(type(num6))
